93ebb3c6b3405b876602283f46ac5cb8c776ad9a
Sayumi <sayumi.nkmr.2019@gmail.com>
2019-11-27 14:44:42 -0800

    shorter code

--- todoList/app.js
+++ todoList/app.js
@@ -2,34 +2,29 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set('view engine', "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+const day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/",function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if(req.body.list === "Work"){
     workItems.push(item);
@@ -45,11 +40,6 @@ app.get("/work", function(req,res){
   res.render("list",{listTitle: "Work List", newListItems: workItems});
 });
 
-// app.post("/work", function(req,res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
 
 app.get("/about", function(req, res){
   res.render("about");
