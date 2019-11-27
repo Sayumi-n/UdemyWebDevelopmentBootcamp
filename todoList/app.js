32ce911141c64e1bbd3a73b9e984ed9bd427f113
Sayumi <sayumi.nkmr.2019@gmail.com>
2019-11-27 14:01:19 -0800

    header and footer added

--- todoList/app.js
+++ todoList/app.js
@@ -51,6 +51,9 @@ app.get("/work", function(req,res){
 //   res.redirect("/work");
 // });
 
+app.get("/about", function(req, res){
+  res.render("about");
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
