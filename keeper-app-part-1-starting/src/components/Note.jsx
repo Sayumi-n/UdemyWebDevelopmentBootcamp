411618b753685d3612fc8cccc4bff4009030281f
Sayumi <sayumi.nkmr.2019@gmail.com>
2019-12-30 15:56:13 -0800

    keeper app update

---
+++ keeper-app-part-1-starting/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (
+    <div className="noteName">
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum autem aliquid nesciunt magni esse optio voluptatum omnis rerum libero dolores provident dolore, tenetur ea modi tempora repudiandae voluptate non fuga.</p>
+    </div>
+    );
+}
+export default Note;
